5d750fa4259cf4fa284b46853964e4dc7e618d9f
Danniel Navas <dannielnavas@gmail.com>
2026-01-23 07:51:27 -0500

    Add user input functionality to greet by name

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -1,7 +1,14 @@
 package platzi.play;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde mi primer proyecto en Java");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, ingresa tu nombre:");
+        String nombre = scanner.nextLine();
+        System.out.println("¡Hola, " + nombre + "! Bienvenido a Platzi Play.");
+        scanner.close();
     }
 }
