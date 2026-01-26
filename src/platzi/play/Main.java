4eb2ee9c2a30d5d5edeb0b9706cc7a04c0538202
Danniel Navas <dannielnavas@gmail.com>
2026-01-26 07:11:32 -0500

    Add XML configuration for migration state and enhance greeting functionality

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo desde mi primer proyecto en Java");
+        System.out.println("Platzi Play - Tu plataforma de entretenimiento favorita");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Por favor, ingresa tu nombre:");
         String nombre = scanner.nextLine();
         System.out.println("¡Hola, " + nombre + "! Bienvenido a Platzi Play.");
+        System.out.println(nombre + ", Cuantos años tienes?");
+        int edad = scanner.nextInt();
+        System.out.println(nombre +", puedes ver contenido +"+ edad);
         scanner.close();
     }
 }
