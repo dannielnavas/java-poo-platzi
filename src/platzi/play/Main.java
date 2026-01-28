74a3b35da51d8c8e9da7d2099c44a75e8b91b15b
Danniel Navas <dannielnavas@gmail.com>
2026-01-28 07:26:03 -0500

    Add version constant to Main class

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main {
+    public static final String VERSION = "1.0.0";
     public static void main(String[] args) {
         System.out.println("Platzi Play - Tu plataforma de entretenimiento favorita");
 
