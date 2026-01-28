ef85da68a12a30a61e282273fef32ca376c0c025
Danniel Navas <dannielnavas@gmail.com>
2026-01-28 07:33:13 -0500

    Add platform name constant and update output format in Main class; refactor ScannerUtils to use final Scanner instance

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -10,8 +10,9 @@ import java.util.Scanner;
 
 public class Main {
     public static final String VERSION = "1.0.0";
+    public static  final String NOMBRE_PLATAFORMA = "Platzi Play";
     public static void main(String[] args) {
-        System.out.println("Platzi Play - Tu plataforma de entretenimiento favorita");
+        System.out.println(NOMBRE_PLATAFORMA + " v" + VERSION);
 
         String nombre = ScannerUtils.capturarTexto("Nombre del contenido: ");
         String generoDeContenido = ScannerUtils.capturarTexto("Género del contenido: ");
